4bd98dee60e6da6dc02e3ab64a8db9b586800e8b
sabadoryo <eabilbay@gmail.com>
2022-05-08 21:48:07 +0600

    leaderboard page

--- src/components/leaderboard/leaderboard.js
+++ src/components/leaderboard/leaderboard.js
@@ -5,38 +5,19 @@ export const Leaderboard = () => {
   return (
     <TableContainer>
       <Table variant='simple'>
-          <TableCaption>Imperial to metric conversion factors</TableCaption>
+          <TableCaption>Top</TableCaption>
           <Thead>
             <Tr>
-              <Th>To convert</Th>
-              <Th>into</Th>
-              <Th isNumeric>multiply by</Th>
+              <Th>username</Th>
+              <Th isNumeric>sr</Th>
             </Tr>
           </Thead>
         <Tbody>
             <Tr>
-              <Td>inches</Td>
-              <Td>millimetres (mm)</Td>
-              <Td isNumeric>25.4</Td>
-            </Tr>
-            <Tr>
-              <Td>feet</Td>
-              <Td>centimetres (cm)</Td>
-              <Td isNumeric>30.48</Td>
-            </Tr>
-            <Tr>
-              <Td>yards</Td>
-              <Td>metres (m)</Td>
-              <Td isNumeric>0.91444</Td>
+              <Td>test</Td>
+              <Td isNumeric>10000</Td>
             </Tr>
           </Tbody>
-          <Tfoot>
-            <Tr>
-              <Th>To convert</Th>
-              <Th>into</Th>
-              <Th isNumeric>multiply by</Th>
-            </Tr>
-        </Tfoot>
       </Table>
   </TableContainer>
   )
